e26397fd4243282c00232a1c69ebbb35d1444acf
Finn Bear <finnbearlabs@gmail.com>
2021-06-20 14:39:51 -0700

    Reduce volume of sounds.

--- assets/sounds/convert.go
+++ assets/sounds/convert.go
@@ -48,24 +48,29 @@ var sounds = map[string]Sound{
 		Source: "freesound.org/479512__craigsmith__r03-04-airplane-engine-steady.wav",
 		Start:  3.745,
 		End:    6.420,
+		Volume: -1,
 	},
 	"alarmSlow": {
 		Source: "freesound.org/165504__ryanconway__missile-lock-detected.mp3",
 		End:    1.243,
+		Volume: -2,
 	},
 	"alarmFast": {
 		Source: "freesound.org/189327__alxy__missile-lock-on-sound.mp3",
 		End:    0.641,
+		Volume: -2.5,
 	},
 	"collect": {
 		Source: "freesound.org/512216__saviraz__coins.mp3",
 		Start:  0.065,
 		End:    0.267,
+		Volume: -1,
 	},
 	"dive": {
 		Source: "freesound.org/480002__craigsmith__r18-31-old-car-ahooga-horn.wav",
 		Start:  2.85,
 		End:    4.75,
+		Volume: -2.5,
 	},
 	/*
 		"dive1": {
@@ -77,76 +82,85 @@ var sounds = map[string]Sound{
 	"explosionShort": {
 		Source: "freesound.org/514647__david2317__03-gran-explosion.wav",
 		Start:  2.471,
-		Volume: -2,
+		Volume: -4,
 	},
 	"explosionLong": {
 		Source: "freesound.org/235968__tommccann__explosion-01.wav",
 		Start:  0.317,
 		End:    6,
-		Volume: -2,
+		Volume: -5,
 	},
 	"horn": {
 		Source: "freesound.org/532339__reznik-krkovicka__horn-mild.mp3",
 		Start:  1.328,
 		End:    5.588,
+		Volume: -1,
 	},
 	"impact": {
 		Source: "freesound.org/4366__qubodup__military-sounds/67468__qubodup__howitzer-gun-impacts-1.flac",
 		End:    0,
-		Volume: -2,
+		Volume: -3,
 		Pitch: -1,
 	},
 	"ocean": {
 		Source: "freesound.org/372181__amholma__ocean-noise-surf.wav",
+		Volume: -1,
 	},
 	"rocket": {
 		Source: "freesound.org/4366__qubodup__military-sounds/67541__qubodup__bgm-71-tow-missile-launch-1.flac",
-		Volume: -1,
+		Volume: -3,
 	},
 	"damage": {
 		Source: "freesound.org/321485__dslrguide__rough-metal-scrape-textured.wav",
 		Pitch: -0.5,
+		Volume: -3,
 	},
 	"shell": {
 		Source: "freesound.org/4366__qubodup__military-sounds/162365__qubodup__navy-battleship-soundscape-turret-gunshots-mechanical-engine-humm-radio-chatter-officer-command-voices.flac",
 		Start:  0.057,
 		End:    2,
-		Volume: -1.5,
+		Volume: -4,
 	},
 	"sonar0": {
 		Source: "freesound.org/90340__digit-al__sonar.wav",
 		End:    5,
+		Volume: -2.5,
 	},
 	"sonar1": {
 		Source: "freesound.org/493162__breviceps__submarine-sonar.wav",
 		Start:  0.184,
 		End:    1.964,
+		Volume: -3,
 	},
 	"sonar2": {
 		Source: "freesound.org/38702__elanhickler__archi-sonar-03.wav",
 		End:    2.5,
+		Volume: -1,
 	},
 	"sonar3": {
 		Source: "freesound.org/70299__kizilsungur__sonar.wav",
+		Volume: -2,
 	},
 	"surface": {
 		Source: "freesound.org/416079__davidlay1__shaving-cream-can-release.wav",
 		End:    2,
+		Volume: -3,
 	},
 	"splash": {
 		Source: "freesound.org/398032__swordofkings128__splash.wav",
-		Volume: -2,
+		Volume: -3.5,
 	},
 	"torpedoLaunch": {
 		Source: "freesound.org/367125__jofae__air-hiss.mp3",
 		Pitch:  -0.25,
-		Volume: -2,
+		Volume: -3,
 	},
 	"upgrade": {
 		Source: "opengameart.org/Rise05.aif",
 		URL: "https://opengameart.org/content/level-up-power-up-coin-get-13-sounds",
 		Start:  0.809,
 		End:    1.4,
+		Volume: -2,
 	},
 }
 
