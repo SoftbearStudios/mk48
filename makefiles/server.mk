1c556cb6af5d94b11c4d0a1e26a4bf05b11dd132
Finn Bear <finntbear@gmail.com>
2025-08-10 17:38:57 -0700

    Update server.mk

--- makefiles/server.mk
+++ makefiles/server.mk
@@ -7,30 +7,30 @@
 .PHONY: debug deploy release run run_debug run_release
 
 debug:
-        cargo build
+	cargo build
 
 deploy: release
 ifdef GAME_ID
-        aws s3 cp target/x86_64-unknown-linux-musl/release/server s3://plasma-prod-binaries/$(GAME_ID) --profile terraform
+	aws s3 cp target/x86_64-unknown-linux-musl/release/server s3://plasma-prod-binaries/$(GAME_ID) --profile terraform
 endif
 
 network_latency: network_repair
-        # one-way latency, jitter, correlation, loss, corruption
-        sudo tc qdisc add dev lo root netem delay 100ms 10ms 25% loss 2% corrupt 0%
+	# one-way latency, jitter, correlation, loss, corruption
+	sudo tc qdisc add dev lo root netem delay 100ms 10ms 25% loss 2% corrupt 0%
 
 network_repair:
-        -sudo tc qdisc delete dev lo root
+	-sudo tc qdisc delete dev lo root
 
 release:
-        cargo build --release --target x86_64-unknown-linux-musl
+	cargo build --release --target x86_64-unknown-linux-musl
 
 run: run_release
 
 run_debug:
-        cargo run -- --client-authenticate-burst 999999 --http-bandwidth-limit 99999999
+	cargo run -- --client-authenticate-burst 999999 --http-bandwidth-limit 99999999
 
 run_debug_cpu_profile:
-        cargo run -- --cpu-profile
+	cargo run -- --cpu-profile
 
 run_release:
-        cargo run --release --target x86_64-unknown-linux-musl -- --client-authenticate-burst 999999 --http-bandwidth-limit 99999999
+	cargo run --release --target x86_64-unknown-linux-musl -- --client-authenticate-burst 999999 --http-bandwidth-limit 99999999
