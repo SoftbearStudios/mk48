7951c4971d9fdc60360333013a24b9549b44db76
Finn Bear <finntbear@gmail.com>
2025-08-10 17:38:13 -0700

    Update client.mk

--- makefiles/client.mk
+++ makefiles/client.mk
@@ -11,36 +11,36 @@ TRUNK_OPTS = --minify --no-sri --skip-version-check --filehash false
 
 
 clean: remove_phrases
-        cargo clean
+	cargo clean
 
 debug:
-        trunk build $(TRUNK_OPTS) index.dev.html
-        mv dist/index.dev.html dist/index.html
+	trunk build $(TRUNK_OPTS) index.dev.html
+	mv dist/index.dev.html dist/index.html
 
 features:
-        clear
-        trunk build index.dev.html --all-features
+	clear
+	trunk build index.dev.html --all-features
 
 licenses:
-        cargo run --manifest-path ../engine/licensing/Cargo.toml -- --binary ../client --binary ../server --format md > src/ui/translations/licenses.md
+	cargo run --manifest-path ../engine/licensing/Cargo.toml -- --binary ../client --binary ../server --format md > src/ui/translations/licenses.md
 
 release:
-        trunk build --release $(TRUNK_OPTS)
+	trunk build --release $(TRUNK_OPTS)
 
 release_std:
-        CARGO_UNSTABLE_BUILD_STD=std,panic_abort CARGO_UNSTABLE_BUILD_STD_FEATURES=panic_immediate_abort trunk build --release $(TRUNK_OPTS)
+	CARGO_UNSTABLE_BUILD_STD=std,panic_abort CARGO_UNSTABLE_BUILD_STD_FEATURES=panic_immediate_abort trunk build --release $(TRUNK_OPTS)
 
 release_std_log:
-        CARGO_UNSTABLE_BUILD_STD=std,panic_abort CARGO_UNSTABLE_BUILD_STD_FEATURES=panic_immediate_abort trunk build --release --features log $(TRUNK_OPTS)
+	CARGO_UNSTABLE_BUILD_STD=std,panic_abort CARGO_UNSTABLE_BUILD_STD_FEATURES=panic_immediate_abort trunk build --release --features log $(TRUNK_OPTS)
 
 remove_phrases:
-        rm -f ../client/phrases.txt ../engine/client/phrases.txt
+	rm -f ../client/phrases.txt ../engine/client/phrases.txt
 
 touch_engine:
-        touch ../engine/client/src/lib.rs
+	touch ../engine/client/src/lib.rs
 
 translations: remove_phrases touch_engine release
 ifdef GAME_ID
-        sort ../client/phrases.txt | uniq | cargo run --manifest-path ../engine/uploader/Cargo.toml -- $(GAME_ID)
+	sort ../client/phrases.txt | uniq | cargo run --manifest-path ../engine/uploader/Cargo.toml -- $(GAME_ID)
 endif
-        sort ../engine/client/phrases.txt | uniq | cargo run --manifest-path ../engine/uploader/Cargo.toml -- Engine
+	sort ../engine/client/phrases.txt | uniq | cargo run --manifest-path ../engine/uploader/Cargo.toml -- Engine
