4764b2e020323eddc014f7d8298a59e27b296859
Finn Bear <finntbear@gmail.com>
2025-08-10 17:39:12 -0700

    Update game.mk

--- makefiles/game.mk
+++ makefiles/game.mk
@@ -9,41 +9,41 @@
 all: fmt
 
 engine/.git:
-        make submodules
+	make submodules
 
 fmt:
-        cargo fmt --manifest-path client/Cargo.toml
-        cargo fmt --manifest-path common/Cargo.toml
-        if [ -d "~/macros" ]; then cargo fmt --manifest-path macros/Cargo.toml; fi
-        if [ -d "~/scene" ]; then cargo fmt --manifest-path scene/Cargo.toml; fi
-        cargo fmt --manifest-path server/Cargo.toml
-        if [ -d "~/server_fuzzer" ]; then cargo fmt --manifest-path server_fuzzer/Cargo.toml; fi
-        if [ -d "~/sprite_sheet_packer" ]; then cargo fmt --manifest-path sprite_sheet_packer/Cargo.toml; fi
+	cargo fmt --manifest-path client/Cargo.toml
+	cargo fmt --manifest-path common/Cargo.toml
+	if [ -d "~/macros" ]; then cargo fmt --manifest-path macros/Cargo.toml; fi
+	if [ -d "~/scene" ]; then cargo fmt --manifest-path scene/Cargo.toml; fi
+	cargo fmt --manifest-path server/Cargo.toml
+	if [ -d "~/server_fuzzer" ]; then cargo fmt --manifest-path server_fuzzer/Cargo.toml; fi
+	if [ -d "~/sprite_sheet_packer" ]; then cargo fmt --manifest-path sprite_sheet_packer/Cargo.toml; fi
 
 run_debug: engine/.git
-        clear
-        (cd client; make debug) && (cd server; make run_debug)
+	clear
+	(cd client; make debug) && (cd server; make run_debug)
 
 run_release: engine/.git
-        clear
-        (cd client; make release) && (cd server; make run_release)
+	clear
+	(cd client; make release) && (cd server; make run_release)
 
 rustup:
-        rustup override set nightly-2024-04-20
-        rustup target add wasm32-unknown-unknown
+	rustup override set nightly-2024-04-20
+	rustup target add wasm32-unknown-unknown
 
 trunk:
-        cargo install --locked trunk --version 0.21.7
-        (cd client; trunk clean --tools)
+	cargo install --locked trunk --version 0.21.7
+	(cd client; trunk clean --tools)
 
 tools: rustup trunk
 
 submodules:
-        git submodule init
-        git submodule update
+	git submodule init
+	git submodule update
 
 clean:
-        cargo clean --manifest-path engine/Cargo.toml
-        cargo clean --manifest-path client/Cargo.toml
-        cargo clean --manifest-path common/Cargo.toml
-        cargo clean --manifest-path server/Cargo.toml
+	cargo clean --manifest-path engine/Cargo.toml
+	cargo clean --manifest-path client/Cargo.toml
+	cargo clean --manifest-path common/Cargo.toml
+	cargo clean --manifest-path server/Cargo.toml
